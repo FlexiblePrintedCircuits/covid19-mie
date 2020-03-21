6e3a43039cbc20c50a6438ea0ce17c23b131e99e
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2020-03-21 20:53:50 +0900

    add nuxt-i18n.config.ts and change nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 import { Configuration } from '@nuxt/types'
+import i18n from './nuxt-i18n.config'
 const path = require('path')
 const purgecss = require('@fullhuman/postcss-purgecss')
 const autoprefixer = require('autoprefixer')
